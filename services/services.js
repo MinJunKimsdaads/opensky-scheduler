b2eb73cb3cd60603919323feaf9104d4c1963ae0
MinJunKimsdaads <93234895+MinJunKimsdaads@users.noreply.github.com>
2025-08-06 11:17:28 +0900

    수정

--- services/services.js
+++ services/services.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import fs from 'fs';
 import path from "path";
 import ftp from 'basic-ftp';
-import {TOKEN_URL,AIRCRAFT_ALL_URL} from '../constant/apiConstant';
+import {TOKEN_URL,AIRCRAFT_ALL_URL} from '../constant/apiConstant.js';
 
 //opensky file open token
 const getAccessToken = async () => {
